a9c6cdda808dd4b4441753582bc4b6d2ebd944d6
donniewang <DonnieWang@qq.com>
2023-03-12 20:46:39 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,59 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <header>
+      <a @click="native('/home')">home</a>&nbsp;|&nbsp;<a
+        @click="native('/profile')"
+        >profile</a
+      >&nbsp;|&nbsp;<a @click="">logout</a>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer></footer>
   </div>
 </template>
 
 <script>
-export default {};
+import { route } from 'vue-router';
+
+export default {
+  setup() {
+    function native(path) {
+      this.$router.push(path);
+    }
+
+    return { native };
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+
 #app {
+  width: 100%;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+header {
+  background-color: rgba(0, 0, 0, 0.3);
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
